04fb4fdc1967b0911c457c39a0cea5106c102a8d
Shubham Kumar Sahu <sdimension0506@gmail.com>
2022-11-09 09:59:45 +0530

    Dashboard UI setup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,33 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Spinner from "../components/spinner";
+
 function Dashboard() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
   return (
-    <div>Dashboard</div>
+    <>
+    <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <p>Contacts Dashboard</p>
+    </section>
+
+    {/* <ContactForm /> */}
+    <section className="content">
+      {/* {contacts.length > 0 ? (
+        <div className="goals">
+          {contacts.map((contact) => (
+            <ContactItem key={contact._id} contact={contact} />
+          ))}
+        </div>
+      ) : */(
+        <h3>You have not created any contacts</h3>
+      )}
+    </section>
+  </>
   )
 }
 
