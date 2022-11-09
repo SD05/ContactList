8d27a94f17751d1093f541c2821c237bbe470659
Shubham Kumar Sahu <sdimension0506@gmail.com>
2022-11-09 09:30:25 +0530

    User Route connected with frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { logout, reset } from "../features/auth/authSlice";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-//   const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
 
   const onLogout = () => {
-    // dispatch(logout());
-    // dispatch(reset());
+    dispatch(logout());
+    dispatch(reset());
     navigate("/");
   };
   return (
@@ -19,13 +19,13 @@ function Header() {
         <Link to="/">Contact Book</Link>
       </div>
       <ul>
-        {/* {user ? (
+        {user ? (
           <li>
             <button className="btn" onClick={onLogout}>
               <FaSignOutAlt /> Logout
             </button>
           </li>
-        ) : */(
+        ) : (
           <>
             <li>
               <Link to="/login">
